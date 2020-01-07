7a9425be0aaa416711b1ef635bf41cad3198f9cc
tangyanglai <44385651+tangyanglai@users.noreply.github.com>
2020-01-07 11:02:21 +0800

    首次提交

---
+++ src/Sms.Core/ISmsTemplateSender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sms.Core
+{
+    public interface ISmsTemplateSender
+    {
+        ISmsService SmsService { get; set; }
+    }
+}
